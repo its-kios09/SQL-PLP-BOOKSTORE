64586c67c174596637ae8e918dcd16c0fafb8743
its-kios09 <legacyitsolution@gmail.com>
2025-04-13 21:31:18 +0300

    (fix) fixed build issues

--- bookstore_schema.sql
+++ bookstore_schema.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS bookstore;
 USE bookstore;
 
+-- Table: book_language
 CREATE TABLE book_language (
     language_id INT AUTO_INCREMENT PRIMARY KEY,
     language_name VARCHAR(50) NOT NULL,
