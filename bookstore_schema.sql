3372960bbbb4b7119f2fe0b815149407322e7fd6
its-kios09 <legacyitsolution@gmail.com>
2025-04-13 22:32:09 +0300

    (feat) added book table

--- bookstore_schema.sql
+++ bookstore_schema.sql
@@ -6,4 +6,26 @@ CREATE TABLE book_language (
     language_id INT AUTO_INCREMENT PRIMARY KEY,
     language_name VARCHAR(50) NOT NULL,
     language_code CHAR(2) NOT NULL
+<<<<<<< Updated upstream
+=======
+);
+
+-- Table : publisher table
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100)
+);
+
+-- Table: book table
+CREATE TABLE book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    isbn VARCHAR(20) UNIQUE,
+    publisher_id INT,
+    language_id INT,
+    num_pages INT,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT DEFAULT 0
+>>>>>>> Stashed changes
 );
